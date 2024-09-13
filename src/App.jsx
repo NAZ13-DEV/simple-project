58c442f6d7229b588f1ed5e6b80f17d571382cf3
NAZ13-DEV <fnwayalani@gmail.com>
2024-09-13 10:41:03 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Counter from "./components/Counter"
 import StarRating from "./components/StarRating";
+import Test from "./components/Test";
 
 const App = () => {
 
@@ -8,7 +9,9 @@ const App = () => {
 
   return (
    <div>
-    <StarRating maxRating={5}/>
+    <StarRating maxRating={5} size={48} className="" messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]} defaultRating={3}/>
+    <StarRating color="red" defaultRating={3} />
+    <Test/>
    </div>
   )
 }
--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -1,23 +1,44 @@
 import { useState } from "react";
 import Star from "./Star";
 
-const StarRating = ({ maxRating = 5 }) => {
-  const [rating, setRating] = useState(1);
+const StarRating = ({ maxRating = 5, color="#ffd700" ,size= 48, className="", messages=[], defaultRating=0 , onSetRating }) => {
+  const [rating, setRating] = useState(defaultRating);
   const [tempRating, setTempRating] = useState(0);
+  const stylingContainer={
+    display:"flex",
+    gap:"16px",
+    alignItems: "center",
+  }
+  const smallerDiv={
+    display:"flex",
+    gap:"4px",
+  }
+  const textStyle ={
+    margin: "0",
+    lineHeight: "1",
+    color,
+    fontSize: `${size / 1.5}px`
+  }
+  function handleRating(rating){
+    setRating(rating);
+    onSetRating(rating);
+  }
   return (
-    <div className="flex items-center gap-[16px]">
-      <div className="flex gap-1">
+    <div style={stylingContainer} className={className}>
+      <div style={smallerDiv}>
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
             key={i}
-            onRate={() => setRating(i + 1)}
+            onRate={() => handleRating(i + 1)}
             full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
             onHoverIn={() => setTempRating(i +1)}
             onHoverOut={() => setTempRating(0)}
+            color={color}
+            size={size}
           />
         ))}
       </div>
-      <p className="m-0 text-2xl">{tempRating || rating || ""}</p>
+      <p style={textStyle}>{messages.length === maxRating ? messages[tempRating ? tempRating - 1 : rating - 1] : tempRating || rating || ""}</p>
     </div>
   );
 };
